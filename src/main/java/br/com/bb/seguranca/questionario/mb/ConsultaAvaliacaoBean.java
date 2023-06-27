f2f1ae510c892c9aecdcf6562226219ae3482c4a
Aldecir Neves Lima <aldecirlima@gmail.com>
2023-06-27 17:57:38 -0300

    Página de consultas otimizada e corrigida a rota

--- src/main/java/br/com/bb/seguranca/questionario/mb/ConsultaAvaliacaoBean.java
+++ src/main/java/br/com/bb/seguranca/questionario/mb/ConsultaAvaliacaoBean.java
@@ -2,15 +2,15 @@ package br.com.bb.seguranca.questionario.mb;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.primefaces.PrimeFaces;
 
 import br.com.bb.seguranca.questionario.modelo.form.Avaliacao;
 import br.com.bb.seguranca.questionario.modelo.form.Opcao;
@@ -40,6 +40,8 @@ public class ConsultaAvaliacaoBean implements Serializable {
 	@Inject
 	private OpcaoService opcaoService;
 
+	private Map<Long, Avaliacao> mapAvaliacoes;
+
 	private Long idLong = null;
 
 	private Avaliacao objAvaliacao;
@@ -58,122 +60,125 @@ public class ConsultaAvaliacaoBean implements Serializable {
 	public void carregaAvaliacao() {
 
 		if (idLong == null) {
-			System.out.println(idLong);
 			return;
 		}
+		
+		if (mapAvaliacoes == null) {
+			mapAvaliacoes = new HashMap<>();
+		}
 
-		objAvaliacao = avaliacaoService.findById(idLong);
+		if (mapAvaliacoes.get(idLong) != null) {
+			objAvaliacao = mapAvaliacoes.get(idLong);
+		} else {
 
-		for (Secao secao : objAvaliacao.getSecoes()) {
-			secao = secaoService.findSecaoId(secao.getIdSecao());
-			System.out.println(secao.getAvaliacao());
-			for (Pergunta pergunta : secao.getPerguntas()) {
-				pergunta = perguntaService.findPerguntaId(pergunta.getIdPergunta());
+			objAvaliacao = avaliacaoService.findById(idLong);
 
-				if (pergunta.getPergunta().getOpcoesParaSelecao() != null) {
-					List<Opcao> list01 = new ArrayList<>();
-					for (Opcao opcao : pergunta.getPergunta().getOpcoesParaSelecao()) {
-						opcao = opcaoService.findById(opcao.getIdOpcao());
-						list01.add(opcao);
-					}
-					pergunta.getPergunta().setOpcoesParaSelecao(list01);
-				}
+			for (Secao secao : objAvaliacao.getSecoes()) {
+				secao = secaoService.findSecaoId(secao.getIdSecao());
+				for (Pergunta pergunta : secao.getPerguntas()) {
+					pergunta = perguntaService.findPerguntaId(pergunta.getIdPergunta());
 
-				if (pergunta.getResposta().getOpcoesSelecionadas() != null) {
-					List<Opcao> list01 = new ArrayList<>();
-					for (Opcao opcao : pergunta.getResposta().getOpcoesSelecionadas()) {
-						opcao = opcaoService.findById(opcao.getIdOpcao());
-						list01.add(opcao);
-					}
-					pergunta.getResposta().setOpcoesSelecionadas(list01);
-				}
-				if (pergunta.getPergunta().getOpcoesParaSelecao() != null) {
-					List<Opcao> list01 = new ArrayList<>();
-					for (Opcao opcao : pergunta.getPergunta().getOpcoesParaSelecao()) {
-						opcao = opcaoService.findById(opcao.getIdOpcao());
-						list01.add(opcao);
-					}
-					pergunta.getPergunta().setOpcoesParaSelecao(list01);
-				}
-
-				System.out.println(pergunta.getResposta().getIdResposta());
-				for (Pergunta perguntaN2 : pergunta.getSubPerguntas()) {
-					perguntaN2 = perguntaService.findPerguntaId(perguntaN2.getIdPergunta());
-
-					if (perguntaN2.getPergunta().getOpcoesParaSelecao() != null) {
+					if (pergunta.getPergunta().getOpcoesParaSelecao() != null) {
 						List<Opcao> list01 = new ArrayList<>();
-						for (Opcao opcao : perguntaN2.getPergunta().getOpcoesParaSelecao()) {
+						for (Opcao opcao : pergunta.getPergunta().getOpcoesParaSelecao()) {
 							opcao = opcaoService.findById(opcao.getIdOpcao());
 							list01.add(opcao);
 						}
-						perguntaN2.getPergunta().setOpcoesParaSelecao(list01);
+						pergunta.getPergunta().setOpcoesParaSelecao(list01);
 					}
 
-					if (perguntaN2.getResposta().getOpcoesSelecionadas() != null) {
+					if (pergunta.getResposta().getOpcoesSelecionadas() != null) {
 						List<Opcao> list01 = new ArrayList<>();
-						for (Opcao opcao : perguntaN2.getResposta().getOpcoesSelecionadas()) {
+						for (Opcao opcao : pergunta.getResposta().getOpcoesSelecionadas()) {
 							opcao = opcaoService.findById(opcao.getIdOpcao());
 							list01.add(opcao);
 						}
-						perguntaN2.getResposta().setOpcoesSelecionadas(list01);
+						pergunta.getResposta().setOpcoesSelecionadas(list01);
 					}
-					
-					if (perguntaN2.getPergunta().getOpcoesParaSelecao() != null) {
+					if (pergunta.getPergunta().getOpcoesParaSelecao() != null) {
 						List<Opcao> list01 = new ArrayList<>();
-						for (Opcao opcao : perguntaN2.getPergunta().getOpcoesParaSelecao()) {
+						for (Opcao opcao : pergunta.getPergunta().getOpcoesParaSelecao()) {
 							opcao = opcaoService.findById(opcao.getIdOpcao());
 							list01.add(opcao);
 						}
-						perguntaN2.getPergunta().setOpcoesParaSelecao(list01);
+						pergunta.getPergunta().setOpcoesParaSelecao(list01);
 					}
 
-					System.out.println(perguntaN2.getResposta().getIdResposta());
+					for (Pergunta perguntaN2 : pergunta.getSubPerguntas()) {
+						perguntaN2 = perguntaService.findPerguntaId(perguntaN2.getIdPergunta());
 
-					for (Pergunta perguntaN3 : perguntaN2.getSubPerguntas()) {
-						perguntaN3 = perguntaService.findPerguntaId(perguntaN3.getIdPergunta());
-						if (perguntaN3.getPergunta().getOpcoesParaSelecao() != null) {
+						if (perguntaN2.getPergunta().getOpcoesParaSelecao() != null) {
 							List<Opcao> list01 = new ArrayList<>();
-							for (Opcao opcao : perguntaN3.getPergunta().getOpcoesParaSelecao()) {
+							for (Opcao opcao : perguntaN2.getPergunta().getOpcoesParaSelecao()) {
 								opcao = opcaoService.findById(opcao.getIdOpcao());
 								list01.add(opcao);
 							}
-							perguntaN3.getPergunta().setOpcoesParaSelecao(list01);
+							perguntaN2.getPergunta().setOpcoesParaSelecao(list01);
 						}
-						if (perguntaN3.getResposta().getOpcoesSelecionadas() != null) {
+
+						if (perguntaN2.getResposta().getOpcoesSelecionadas() != null) {
 							List<Opcao> list01 = new ArrayList<>();
-							for (Opcao opcao : perguntaN3.getResposta().getOpcoesSelecionadas()) {
+							for (Opcao opcao : perguntaN2.getResposta().getOpcoesSelecionadas()) {
 								opcao = opcaoService.findById(opcao.getIdOpcao());
 								list01.add(opcao);
 							}
-							perguntaN3.getResposta().setOpcoesSelecionadas(list01);
+							perguntaN2.getResposta().setOpcoesSelecionadas(list01);
 						}
-						
-						if (perguntaN3.getPergunta().getOpcoesParaSelecao() != null) {
+
+						if (perguntaN2.getPergunta().getOpcoesParaSelecao() != null) {
 							List<Opcao> list01 = new ArrayList<>();
-							for (Opcao opcao : perguntaN3.getPergunta().getOpcoesParaSelecao()) {
+							for (Opcao opcao : perguntaN2.getPergunta().getOpcoesParaSelecao()) {
 								opcao = opcaoService.findById(opcao.getIdOpcao());
 								list01.add(opcao);
 							}
-							perguntaN3.getPergunta().setOpcoesParaSelecao(list01);
+							perguntaN2.getPergunta().setOpcoesParaSelecao(list01);
 						}
 
+						for (Pergunta perguntaN3 : perguntaN2.getSubPerguntas()) {
+							perguntaN3 = perguntaService.findPerguntaId(perguntaN3.getIdPergunta());
+							if (perguntaN3.getPergunta().getOpcoesParaSelecao() != null) {
+								List<Opcao> list01 = new ArrayList<>();
+								for (Opcao opcao : perguntaN3.getPergunta().getOpcoesParaSelecao()) {
+									opcao = opcaoService.findById(opcao.getIdOpcao());
+									list01.add(opcao);
+								}
+								perguntaN3.getPergunta().setOpcoesParaSelecao(list01);
+							}
+							if (perguntaN3.getResposta().getOpcoesSelecionadas() != null) {
+								List<Opcao> list01 = new ArrayList<>();
+								for (Opcao opcao : perguntaN3.getResposta().getOpcoesSelecionadas()) {
+									opcao = opcaoService.findById(opcao.getIdOpcao());
+									list01.add(opcao);
+								}
+								perguntaN3.getResposta().setOpcoesSelecionadas(list01);
+							}
 
-						System.out.println(perguntaN3.getResposta().getIdResposta());
+							if (perguntaN3.getPergunta().getOpcoesParaSelecao() != null) {
+								List<Opcao> list01 = new ArrayList<>();
+								for (Opcao opcao : perguntaN3.getPergunta().getOpcoesParaSelecao()) {
+									opcao = opcaoService.findById(opcao.getIdOpcao());
+									list01.add(opcao);
+								}
+								perguntaN3.getPergunta().setOpcoesParaSelecao(list01);
+							}
+
+						}
 					}
 				}
 			}
 		}
 
+		if (mapAvaliacoes.get(objAvaliacao.getIdAvaliacao()) != objAvaliacao) {
+			mapAvaliacoes.put(objAvaliacao.getIdAvaliacao(), objAvaliacao);
+		}
+		
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().getFlash().put("avaliacao", objAvaliacao);
 			FacesContext.getCurrentInstance().getExternalContext().getFlash().put("passagem", parametroPassagem);
 			FacesContext.getCurrentInstance().getExternalContext().redirect("consultaResposta.xhtml");
-			PrimeFaces.current().ajax().update("formConsultaResposta");
-			PrimeFaces.current().ajax().update("buttonVoltar");
 		} catch (Exception e) {
 			FacesMessages.error("Não foi possível carregar a visualização");
 		}
-
 	}
 
 	public void voltaPaginaConsultas() {
@@ -215,6 +220,14 @@ public class ConsultaAvaliacaoBean implements Serializable {
 
 	public void setIdLong(Long idLong) {
 		this.idLong = idLong;
+	}
+
+	public Map<Long, Avaliacao> getMapAvaliacoes() {
+		return mapAvaliacoes;
+	}
+
+	public void setMapAvaliacoes(Map<Long, Avaliacao> mapAvaliacoes) {
+		this.mapAvaliacoes = mapAvaliacoes;
 	}
 
 }
